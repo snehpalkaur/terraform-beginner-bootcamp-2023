8dbf7b0f605738c005eb2134ecea43b9033d39fc
Snehpal Kaur <kausnehp@sheridancollege.ca>
2023-10-06 01:02:59 +0000

    #21 restructure root modules / set up env vars

--- journal/week1.md
+++ journal/week1.md
@@ -17,3 +17,29 @@ PROJECT_ROOT
 
 
 [Standard Module Structure](https://developer.hashicorp.com/terraform/language/modules/develop/structure)
+
+## Terraform and Input Variables
+### Terrform Cloud Variables
+
+In Terraform, variables can set using two types:-
+
+- Environment Variables - would set in bash terminal eg. AWS credentials
+- Terraform Variables - would normally set in `tfvars` files
+
+Terraform Cloud Variables can be set sensitive so that they are not visible in the UI
+
+### Loading Terraform Input Variables
+
+`-var` flag can be used to set an input variable or override a variable in the tfvars file eg. `terraform -var user_uuid="my_user_uuid"`
+
+### var flag
+
+### var-file flag
+
+### terraform.tfvars
+
+### auto.tfvars
+
+### order of terraform variables
+
+## Changing state - remote/local
--- main.tf
+++ main.tf
@@ -1,37 +1,13 @@
-terraform {
-  cloud {
-    organization = "terraform_bootcamp_sneh"
 
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-
-
-provider "aws" {
-  # Configuration options
-}
 
 resource "aws_s3_bucket" "example" {
   #Bucket naming rules
   #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
 
 }
 
@@ -40,10 +16,7 @@ resource "random_string" "bucket_name" {
   upper = false
   length  = 32
   special = false
-}
-
-
-output "random_bucket_name"{
-    value = random_string.bucket_name.result
 
 }
+
+
