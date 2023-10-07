185063a34715aef182bc560918c290af7ac8cc54
Snehpal Kaur <47006792+snehpalkaur@users.noreply.github.com>
2023-10-07 10:49:44 -0500

    21 restructure root module (#23)
    * Update week1.md
    
    * #21 restructure root modules / set up env vars
    
    * #21 restrucutre root module and changed state from remtoe to local

--- providers.tf
+++ providers.tf
@@ -1,11 +1,5 @@
 terraform {
-  cloud {
-    organization = "terraform_bootcamp_sneh"
 
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
   required_providers {
     random = {
       source = "hashicorp/random"
