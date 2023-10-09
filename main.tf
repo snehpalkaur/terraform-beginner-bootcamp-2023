5a5f18e6e6cb71c6978e6b4cb05b8c334f098ca6
Snehpal Kaur <kausnehp@sheridancollege.ca>
2023-10-09 16:56:51 +0000

    #28 enabled static website hosting - uploaded index and error html file  via tf

--- main.tf
+++ main.tf
@@ -19,5 +19,7 @@ module "terrhouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  error_html_filepath = var.error_html_filepath
+  index_html_filepath = var.index_html_filepath
   
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name"{
     description = "Bucket name for our static website hosting"
     value = module.terrhouse_aws.bucket_name
 }
+
+output "s3_website_endpoint"{
+    description = "S3 static website hosting endpoint"
+    value = module.terrhouse_aws.website_endpoint
+}
