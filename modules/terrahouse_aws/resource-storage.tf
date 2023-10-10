9d6ac6c6bb80864eaf09c004675acdffe98d2c5b
Snehpal Kaur <47006792+snehpalkaur@users.noreply.github.com>
2023-10-09 22:54:01 -0500

    #31 only upload index.html/error.html if the content version has change (#33)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -40,6 +40,10 @@ resource "aws_s3_object" "index_html" {
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "aws_s3_object" "error_html" {
@@ -52,6 +56,10 @@ resource "aws_s3_object" "error_html" {
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5(var.error_html_filepath)
+  lifecycle {
+     replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [ etag ]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -75,4 +83,9 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
+  
 }
