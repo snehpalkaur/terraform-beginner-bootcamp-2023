ece4d819c5e67d376b10f2c9d102bd4333f5079d
Snehpal Kaur <47006792+snehpalkaur@users.noreply.github.com>
2023-11-11 12:01:34 -0600

    42 terratowns provider (#43)
    * wip
    
    * #47 setup skeleton for resource

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name"{
-    description = "Bucket name for our static website hosting"
-    value = module.terrhouse_aws.bucket_name
-}
-
-output "s3_website_endpoint"{
-    description = "S3 static website hosting endpoint"
-    value = module.terrhouse_aws.website_endpoint
-}
-
-output "cloudfornt_url" {
-    description = "The Cloudfront Distribution domain name"
-    value = module.terrhouse_aws.cloudfornt_url
-}
+#output "bucket_name"{
+#    description = "Bucket name for our static website hosting"
+#    value = module.terrhouse_aws.bucket_name
+#}
+#
+#output "s3_website_endpoint"{
+#    description = "S3 static website hosting endpoint"
+#    value = module.terrhouse_aws.website_endpoint
+#}
+#
+#output "cloudfornt_url" {
+#    description = "The Cloudfront Distribution domain name"
+#    value = module.terrhouse_aws.cloudfornt_url
+#}
